6e48c8611be5381d150bc09fdce9fbfb5c4ca01a
Sharan Thakur <sharandt19@live.com>
2024-10-19 18:39:38 +0200

    drops JSON approach to represent data, adds `Transferable` `Photo` model

--- QRCodeBadgeGenerator/QRCodeBadgeGenerator/DetailsModel.swift
+++ QRCodeBadgeGenerator/QRCodeBadgeGenerator/DetailsModel.swift
@@ -5,44 +5,49 @@
 //  Created by Sharan Thakur on 16/10/24.
 //
 
-import Foundation
+import SwiftUI
 
-struct PersonalDetails: Identifiable, Hashable {
+struct PersonalDetails: Identifiable, Hashable, CustomStringConvertible {
     let id = UUID()
-    var name: String
-    var emailAddress: String
-    var twitterHandle: String
-    var githubHandle: String
-}
-
-extension PersonalDetails {
-    static var empty: PersonalDetails {
-        PersonalDetails(name: "", emailAddress: "", twitterHandle: "", githubHandle: "")
+    var name: String = ""
+    var emailAddress: String = ""
+    var twitterHandle: String = ""
+    var githubHandle: String = ""
+    
+    var description: String {
+        "\(name)\n\(emailAddress)\n\(twitterHandle)\n\(githubHandle)"
     }
 }
 
 /// Codable Extensions
-extension PersonalDetails: Codable {
-    enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case emailAddress = "email_address"
-        case twitterHandle = "twitter"
-        case githubHandle = "github"
+extension PersonalDetails {
+    /// Method to encode as data
+    /// - Returns: ``Data``
+    func asData() -> Data {
+        Data(self.description.utf8)
+    }
+}
+
+struct Photo: Identifiable, Transferable {
+    let id = UUID()
+    let image: Image
+    let qrImage: Image
+    
+    init(image: Image, qrImage: Image) {
+        self.image = image
+        self.qrImage = qrImage
     }
     
-    /// Initializer from ``Data``
-    /// - Parameter data: `JSON` encoded ``Data``
-    init(fromData data: Data) throws {
-        let details = try JSONDecoder().decode(PersonalDetails.self, from: data)
-        self.emailAddress = details.emailAddress
-        self.twitterHandle = details.twitterHandle
-        self.githubHandle = details.githubHandle
-        self.name = details.name
+    init(uiImage: UIImage, qrUIImage: UIImage) {
+        self.image = Image(uiImage: uiImage)
+        self.qrImage = Image(uiImage: qrUIImage)
     }
     
-    /// Method to encode as `JSON` data
-    /// - Returns: ``Data`` with `JSON` encoded properties
-    func asData() throws -> Data {
-        try JSONEncoder().encode(self)
+    static var transferRepresentation: some TransferRepresentation {
+        ProxyRepresentation(exporting: \.image)
+    }
+    
+    var sharePreview: SharePreview<Image, Never> {
+        SharePreview("QRBadge", image: self.image)
     }
 }
